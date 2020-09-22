275900f08049d946a85eaa90dae1cc3219956de6
AltCtrlDel1999 <shubhamrampurkar3@gmail.com>
2020-09-22 19:02:16 +0530

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,29 +8,30 @@ return(
 
             <div className="container text-center text-md-left">
 
-  
+
                  <div className="row">
 
 
                      <div className="col-md-6  ">
 
-    
+
                          <h5 class="font-weight-bold text-uppercase mt-3 mb-4 ">Social Schedular</h5>
                          <p >A project to automatically send invite to all of your social media like LinkedIn, Facebook, email,
                               WhatsApp, Instagram as soon as he finishes filling the form.</p>
                      </div>
                     <div class="col-md-6 ">
                          <ul className="list-unstyled list-inline text-center py-2">
+
                             <li className="list-inline-item">
-                            <h5 className="mb-1">Register for free</h5>
+                            <Link to="/register">
+                            <a href="#" className="btn btn-danger btn-rounded">Contact Us!</a>
+                            </Link>
                             </li>
-                            <li className="list-inline-item">
-                            <a href="#" className="btn btn-danger btn-rounded">Sign up!</a>
-                            </li>
+
                          </ul>
                     </div>
 
-    
+
                 </div>
 
             </div>
@@ -49,12 +50,12 @@ return(
                  </a>
              </li>
             <li className="list-inline-item">
-              <a className="fb-ic">
+              <a href="https://github.com/anushbhatia/Social-Scheduler" className="fb-ic">
                 <i  className="fa fa-github fa-3x"></i>
                  </a>
              </li>
-  
-  
+
+
         </ul>
 
          <div className="footer-copyright text-center py-3">© 2020 Copyright:
